a53b3610b0f2dae85cee13d517daa78462394f4e
白彩恋 <shiro@oom-wg.dev>
2025-09-05 19:58:58 +0800

    Update

--- cppless.hh
+++ cppless.hh
@@ -16,51 +16,51 @@
 #include <utility>
 
 #define __cppless_concat_impl(a, b) a##b
-#define __cppless_concat(a, b)      __cppless_concat_impl(a, b)
+#define __cppless_concat(a, b)		__cppless_concat_impl(a, b)
 
 #define defer_func(_____)                                                                 \
-    [[maybe_unused]] auto const __cppless_concat(__defer_func__,                          \
-                                                 __COUNTER__) = [&]<typename _>(_&& __) { \
-        struct ___ {                                                                      \
-            _ __;                                                                         \
-            ~___() { __(); }                                                              \
-        } ____{std::forward<_>(__)};                                                      \
-        return ____;                                                                      \
-    }([&]() _____)
+	[[maybe_unused]] auto const __cppless_concat(__defer_func__,                          \
+												 __COUNTER__) = [&]<typename _>(_&& __) { \
+		struct ___ {                                                                      \
+			_ __;                                                                         \
+			~___() { __(); }                                                              \
+		} ____{std::forward<_>(__)};                                                      \
+		return ____;                                                                      \
+	}([&]() _____)
 
 #define let(_, __) ([&](auto&& it) -> decltype(auto) __(_))
 
 #define also(__, ___)                            \
-    ([&]<typename _>(_&& it) -> decltype(auto) { \
-        ___;                                     \
-        return std::forward<_>(it);              \
-    }(__))
+	([&]<typename _>(_&& it) -> decltype(auto) { \
+		___;                                     \
+		return std::forward<_>(it);              \
+	}(__))
 
 template<typename T>
 class when {
 public:
-    explicit when(T const& v): val(v), ok(false) {}
-    template<class F>
-    when& add_case(T const& target, F&& func) {
-        if (!ok && val == target) ok = (func(val), true);
-        return *this;
-    }
-    template<class F>
-    when& add_case(std::initializer_list<T> targets, F&& func) {
-        if (!ok)
-            for (auto const& target : targets)
-                if (val == target) {
-                    ok = (func(val), true);
-                    break;
-                }
-        return *this;
-    }
-    template<class F>
-    void default_case(F&& func) {
-        if (!ok) func(val);
-    }
+	explicit when(T const& v): val(v), ok(false) {}
+	template<class F>
+	when& add_case(T const& target, F&& func) {
+		if (!ok && val == target) ok = (func(val), true);
+		return *this;
+	}
+	template<class F>
+	when& add_case(std::initializer_list<T> targets, F&& func) {
+		if (!ok)
+			for (auto const& target : targets)
+				if (val == target) {
+					ok = (func(val), true);
+					break;
+				}
+		return *this;
+	}
+	template<class F>
+	void default_case(F&& func) {
+		if (!ok) func(val);
+	}
 
 private:
-    T const& val;
-    bool     ok;
+	T const& val;
+	bool	 ok;
 };
