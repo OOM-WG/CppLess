7dbd34c8a4417a9f5aaecd517641b1ee7bfc1e99
白彩恋 <shiro@oom-wg.dev>
2025-10-22 20:42:37 +0800

    Update

--- cppless.hh
+++ cppless.hh
@@ -18,14 +18,15 @@
 #define __cppless_concat_impl(a, b) a##b
 #define __cppless_concat(a, b)		__cppless_concat_impl(a, b)
 
-#define defer_func(_____)                                                                 \
-	[[maybe_unused]] auto const __cppless_concat(__defer_func__,                          \
-												 __COUNTER__) = [&]<typename _>(_&& __) { \
-		struct ___ {                                                                      \
-			_ __;                                                                         \
-			~___() { __(); }                                                              \
-		} ____{std::forward<_>(__)};                                                      \
-		return ____;                                                                      \
+#define defer_func(_____)                                    \
+	[[maybe_unused]]                                         \
+	auto const __cppless_concat(__defer_func__, __COUNTER__) \
+			= [&]<typename _>(_&& __) {                      \
+		struct ___ {                                         \
+			_ __;                                            \
+			~___() { __(); }                                 \
+		} ____ { std::forward<_>(__) };                      \
+		return ____;                                         \
 	}([&]() _____)
 
 #define let(_, __) ([&](auto&& it) -> decltype(auto) __(_))
@@ -38,7 +39,7 @@
 
 template<typename T>
 class when {
-public:
+   public:
 	explicit when(T const& v): val(v), ok(false) {}
 	template<class F>
 	when& add_case(T const& target, F&& func) {
@@ -60,7 +61,7 @@ public:
 		if (!ok) func(val);
 	}
 
-private:
+   private:
 	T const& val;
 	bool	 ok;
 };
